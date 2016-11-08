b3d819526e9d91fe67155c71eeeef2a8e3d3e0c1
artem-yam <rulezzz97@mail.ru>
2016-11-08 19:32:43 +0300

    fix

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ document.write("<p>1 var = "+a+"; 2 var = "+b+"</p>");
 //Задание 2
 
 document.write("<h4>Задание 2</h4>");
-function S_tr(a,h)
-{
+function S_tr(a,h) {
     document.write("<p>Площадь треугольника (сторона = " + a
     + ", высота = " + h + ") : " + (1/2*a*h)+"</p>")
 }
@@ -18,23 +17,24 @@ S_tr(2,2);
 
 document.write("<h4>Задание 3</h4>");
 var arr = [1,2,3];
-function Reverse_array(array)
-{
-    var arr1=[], arr2=[], arr3=[], i = 0;
+function Reverse_array(array) {
+    var i = 0,
+        arr1=[], 
+        arr2=[], 
+        arr3=[];
     document.write("Исходный массив = "+array+"<br>");
-    do
-    {
+    do {
         arr1[i]=array[array.length-1-i];
         i++;
     }   while (i<array.length);
     i=0;
-    while (i<array.length)
-    {
+    while (i<array.length) {
         arr2[i]=array[array.length-1-i];
         i++;
     } 
-    for(i=0;i<array.length;i++)
+    for(i=0;i<array.length;i++) {
         arr3[i]=array[array.length-1-i];
+    }
     document.write("Перевернутые массивы : <br>");
     document.write("Массив 1 = "+arr1+"<br>");
     document.write("Массив 2 = "+arr2+"<br>");
@@ -45,11 +45,10 @@ Reverse_array(arr);
 //Задание 4
 
 document.write("<h4>Задание 4</h4>");
-arr1 = arr; arr2 = arr;
-var i=0, j=0;
+var i=0,
+    j=0;
 document.write("++i " + "&nbsp; i++" + "<br>");
-while ((i<3) && (j<3))
-{
+while ((i<3) && (j<3)) {
     document.write("&nbsp;&nbsp;&nbsp;"+ ++i + "&nbsp;&nbsp; ");
     document.write(j++ + "<br>");
 }
@@ -57,13 +56,14 @@ while ((i<3) && (j<3))
 //Задание 5
 
 document.write("<h4>Задание 5</h4>");
-function number(arg)
-{
+function number(arg) {
     if (arg>0) document.write("Число " + arg + " - положительное<br>")
     else if (arg ==0) document.write("Число " + arg + " - нулевое<br>")
     else document.write("Число " + arg + " - отрицательное<br>")
 }
-number(2); number(0); number(-5);
+number(2); 
+number(0); 
+number(-5);
 
 //Задание 6
 
@@ -76,11 +76,11 @@ document.write("Введено имя : " + name);
 //Задание 7
 
 document.write("<h4>Задание 7</h4>");
-function factorial(num)
-{
+function factorial(num) {
     var fact = 1;
-    for (i=1;i<=num;i++)
-        fact*=i;
+    for (var i=1;i<=num;i++) {
+        fact *= i;
+    }
     document.write("Факториал числа " + num +" = "+ fact + "<br>");
 }
 factorial(3);
